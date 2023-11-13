bb0241b5f1e2b31350a2fac87620af96e3da6e9d
rayhanmp <90276351+rayhanmp@users.noreply.github.com>
2023-11-14 04:32:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from component import router as component_router
 from furniture import router as furniture_router
 from cart import router as cart_router
 from model import router as model_router
+from auth import router as auth_router
 
 app = FastAPI()
 
@@ -12,6 +13,7 @@ app.include_router(component_router, prefix='/component')
 app.include_router(furniture_router, prefix='/furniture')
 app.include_router(cart_router, prefix='/cart')
 app.include_router(model_router, prefix='/model')
+app.include_router(auth_router, prefix='/token')
 
 @app.get('/')
 async def welcome():
